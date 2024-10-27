62ec33581851a8d8e509dc87e54072e05935607a
Soliman-Majam <Soliman.Majam@city.ac.uk>
2024-10-27 17:40:45 +0000

    Commented it out for others

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 
-# reading gym members dataset
-df = pd.read_csv('gym_members_exercise_tracking.csv', na_values=['NA', '?'])
-
-# print first 5 rows to check it works
-print(df[:5])
-print(" ")
-
-# checking to see if there is any missing data
-print(df.isnull().any())
-print(" ")
+# =============================================================================
+# # reading gym members dataset
+# df = pd.read_csv('gym_members_exercise_tracking.csv', na_values=['NA', '?'])
+# 
+# # print first 5 rows to check it works
+# print(df[:5])
+# print(" ")
+# 
+# # checking to see if there is any missing data
+# print(df.isnull().any())
+# print(" ")
+# =============================================================================
