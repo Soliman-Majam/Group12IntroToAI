a7f804ef674ca14cdba9df3478d3dec73374ab5d
Soliman-Majam <Soliman.Majam@city.ac.uk>
2024-10-27 17:35:03 +0000

    Confirmed that gym members dataset has no missing values
    Testing one of our 3 possible datasets we might use, gym_members_excercise_tracking.csv for any missing data. No missing data was found so this might be a good candidate for analysis.

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-#fatima
-#soliman
-#Treisis
-#Bader
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt  
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+# reading gym members dataset
+df = pd.read_csv('gym_members_exercise_tracking.csv', na_values=['NA', '?'])
+
+# print first 5 rows to check it works
+print(df[:5])
+print(" ")
+
+# checking to see if there is any missing data
+print(df.isnull().any())
+print(" ")
