d078a93fc3c2e6f67d5c217d504d367854ed5e49
adck872 <fatima.ahmed.3@city.ac.uk>
2024-10-28 17:20:34 +0000

    heart analysis

--- main.py
+++ main.py
@@ -32,3 +32,17 @@ from sklearn import metrics
 
 #print(df.isnull().any())
 #------------------------------------------------------------------------------
+#Fatima Dataset:
+import pandas as pd
+
+# Load the dataset and handle NA values
+df = pd.read_csv('heart_attack_analysis.csv', na_values=['NA', '?'])
+
+# Print the entire DataFrame
+pd.set_option('display.max_rows', None)  # all rows
+print(df)
+print(" ")
+
+# Checking for missing data in all columns
+print("Missing data in each column:")
+print(df.isnull().any())
