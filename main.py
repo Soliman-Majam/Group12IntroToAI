a2cf4ff595e161923294fd97cbe5196a1fc78ffa
Soliman-Majam <Soliman.Majam@city.ac.uk>
2024-11-04 21:07:07 +0000

    importing relevant libraries and start to perceptron

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt  
 from sklearn.model_selection import train_test_split 
-from sklearn.linear_model import LinearRegression
 from sklearn import metrics
-
+from sklearn.linear_model import Perceptron
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
 # =============================================================================
 # # reading gym members dataset
 # df = pd.read_csv('gym_members_exercise_tracking.csv', na_values=['NA', '?'])
@@ -86,6 +88,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 print("\nAfter splitting:")
 print("Training set - Features (X_train):", X_train.shape, "Target (y_train):", y_train.shape)
 print("Testing set - Features (X_test):", X_test.shape, "Target (y_test):", y_test.shape)
+
+ppn = Perceptron(max_iter=40,tol=0.001,eta0=1)
 
 
 
